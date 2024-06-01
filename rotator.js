74954f5ca377b2cf52b9cd7f72c62e57e3b24ad1
nico110203 <nicolas110203@proton.me>
2024-06-01 19:01:17 +0200

    Update rotator.js

--- rotator.js
+++ rotator.js
@@ -12,7 +12,7 @@ const urlWeights = [
     { url: "https://t.co/Npme9DJsBw", weight: 20 },
     { url: "https://t.co/xqyWcCzwBG", weight: 20 },
     { url: "https://t.co/MyLLqPwi9o", weight: 10 },
-    { url: "https://t.co/C8gqQGHozW", weight: 50 },
+    { url: "https://t.co/GV5jjpg7az", weight: 50 },
     { url: "https://t.co/ogNrrfZ1CN", weight: 50 },
    
 ];
