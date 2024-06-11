ee7acfa40e4452005a9ea2ea1a101c9b1a27ab52
nico110203 <nicolas110203@proton.me>
2024-06-11 17:11:25 +0200

    Update rotator.js

--- rotator.js
+++ rotator.js
@@ -1,27 +1,48 @@
 // List of URLs with their respective weights
 const urlWeights = [
+
+    // Mon Collier Unique
+    
     { url: "https://t.co/iCGhRXO6fU", weight: 10 },
     { url: "https://t.co/JodASXZXWr", weight: 10 },
     { url: "https://t.co/VUdc8qC2u8", weight: 10 },
     { url: "https://t.co/vkilQKSyqL", weight: 10 },
     { url: "https://t.co/XdhkXUtafT", weight: 10 },
-    { url: "https://t.co/uVMVxjhiZX", weight: 10 },
-    { url: "https://t.co/QWgbefZean", weight: 10 },
-    { url: "https://t.co/AmVguTMBSq", weight: 10 },
-    { url: "https://t.co/waNUMqvTfl", weight: 10 },
-    { url: "https://t.co/dJ056vFgVv", weight: 10 },
     
-    { url: "https://t.co/5IfJncj3uQ", weight: 100 },
+    { url: "https://t.co/Gqg3yYsJID", weight: 10 },
+    { url: "https://t.co/TriZVXGvmb", weight: 10 },
+    { url: "https://t.co/hybXWGYfQY", weight: 10 },
+    { url: "https://t.co/USqEcngXRT", weight: 10 },
+    { url: "https://t.co/ANcd3EPiOH", weight: 10 },
+  
+    // Bracelet Photo
     
-    { url: "https://t.co/C8gqQGHozW", weight: 30 },
-    { url: "https://t.co/EHb54L9Rrp", weight: 20 },
-    { url: "https://t.co/Npme9DJsBw", weight: 20 },
-    { url: "https://t.co/xqyWcCzwBG", weight: 20 },
+    { url: "https://t.co/XXZbpiyfkB", weight: 25 },
+    { url: "https://t.co/rLoYiiE4C0", weight: 25 },
+    { url: "https://t.co/kLWKy4xnET", weight: 25 },
+    { url: "https://t.co/OENbRPOswC", weight: 25 },
+    
+    // Maison du Caftan
+    
+    { url: "https://t.co/olxpqDhv25", weight: 10 },
+    { url: "https://t.co/oCqq9bT6Mv", weight: 10 },
+    { url: "https://t.co/GaUDDGvVei", weight: 10 },
+    
+    { url: "https://t.co/EHb54L9Rrp", weight: 10 },
+    { url: "https://t.co/Npme9DJsBw", weight: 10 },
+    { url: "https://t.co/xqyWcCzwBG", weight: 10 },
     { url: "https://t.co/MyLLqPwi9o", weight: 10 },
+
+    { url: "https://t.co/iKfYFaL6BYo", weight: 5 },
+    { url: "https://t.co/Cuwug6jTT3", weight: 5 },
+    { url: "https://t.co/IbM40V5Cdh", weight: 5 },
+    { url: "https://t.co/gBx18Ph8jg", weight: 5 },
+    { url: "https://t.co/lKYbaHzm2K", weight: 5 },
+    { url: "https://t.co/GvcJqDAJLU", weight: 5 },
     
-    { url: "https://t.co/ogNrrfZ1CN", weight: 30 },
+    // Robes de plage
     
-    { url: "https://t.co/GV5jjpg7az", weight: 30 }
+    { url: "https://t.co/tYJt9L6wFt", weight: 30 }
 
     
 ];
