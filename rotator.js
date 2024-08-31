a321eba1a45f50541211190b1e9a0d3663a99bbe
nico110203 <nicolas110203@proton.me>
2024-08-31 11:38:01 +0200

    Update rotator.js
    juste laissé Bracelet Photo

--- rotator.js
+++ rotator.js
@@ -1,38 +1,15 @@
 // List of URLs with their respective weights
 const urlWeights = [
 
-    // Mon Collier Unique
-    
-    { url: "https://t.co/3wB5heQPCU", weight: 10 },
-    { url: "https://t.co/zxpfiwMHVx", weight: 10 },
-    { url: "https://t.co/BgxDvnENMv", weight: 10 },
-    { url: "https://t.co/r9zEcZIKZN", weight: 10 },
-    { url: "https://t.co/ckU41VTIke", weight: 10 },
-    
-    
-    { url: "https://t.co/oPZavxymTK", weight: 10 },
-    { url: "https://t.co/ssjhqRyzVd", weight: 10 },
-    { url: "https://t.co/43IDcCdwTG", weight: 10 },
-    { url: "https://t.co/IhYMeGiiSn", weight: 10 },
-    { url: "https://t.co/FJfiZDBMtP", weight: 10 },
   
     // Bracelet Photo
 
-    { url: "https://t.co/QPMtVYynqy", weight: 25 },
-    { url: "https://t.co/c4FhHtNbQE", weight: 25 },
-    { url: "https://t.co/GiRCF6VXu1", weight: 25 },
-    { url: "https://t.co/XGaylMhJD3", weight: 25 },
+    { url: "https://t.co/nLhf4rc3yH", weight: 1 },
+    { url: "https://t.co/XoxkJtpY6m", weight: 1 },
+    { url: "https://t.co/OhPOCLI9Kg", weight: 1 },
+    { url: "https://t.co/4nx8AAqqfK", weight: 1 },
+    { url: "https://t.co/acPIGII5vl", weight: 1 },
    
-    
-    // Maison du Caftan
-    
-    { url: "https://t.co/tSka5MFWtO", weight: 20 },
-    { url: "https://t.co/aDYk3jQ8Ul", weight: 20 },
-    
-    { url: "https://t.co/5Q7RdS8dO3", weight: 20 },
-    { url: "https://t.co/tpsX8VvTzX", weight: 20 },
-    { url: "https://t.co/JkkAGEcB2u", weight: 10 },
-    { url: "https://t.co/iT7hDOcl7e", weight: 10 },
 
     
 ];
